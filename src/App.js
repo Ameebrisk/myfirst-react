4c3fbc168b8f67618a4082170e9f0337ada3c66a
Amee Brisk <af2633@icloud.com>
2022-07-17 19:32:56 -0600

    homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Child from "./Child";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// return (
+//   <div>
+//     <h3>{props.name}</h3>
+//     {props.children}
+//   </div>
+// )
+// function Child(user) {
+//   return <h3>`Hello {user.fname + " " + user.lname}</h3>;
+// }
+
+export default function App() {
+  const user = {
+    fname: "Amee",
+    lname: "Brisk",
+  };
+
+  return <div className="App">{Child(user)}</div>;
 }
-
-export default App;
---
+++ src/Child.js
@@ -0,0 +1,3 @@
+export default function Child(user) {
+  return <h3>Hello, I am {user.fname + " " + user.lname}</h3>;
+}
